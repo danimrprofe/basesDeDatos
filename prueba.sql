dcee16c7517efd32624605ac90d072f03106f36a
Daniel Moreno <danimrprofe@gmail.com>
2020-12-01 12:10:13 +0100

    Update prueba.sql

--- prueba.sql
+++ prueba.sql
@@ -5,10 +5,18 @@ CREATE TABLE productos (
   Disponible BOOL
   );
 
+# Llenamos la lista de productos
+
 INSERT INTO productos VALUES (1, 'Manzana', 0.20, False);
 INSERT INTO productos VALUES (1, 'Redbull', 0.20, False);
 INSERT INTO productos VALUES (1, 'Cafe', 0.20, True);
 INSERT INTO productos VALUES (1, 'Croissant', 0.20, False);
 INSERT INTO productos VALUES (1, 'Donut', 0.20, True);
 
+# Muestra los productos que hay que comprar
+
 SELECT NombreProducto FROM productos WHERE disponible = False
+
+# Muestra la carta
+
+SELECT NombreProducto, PrecioProducto FROM productos WHERE disponible = False
